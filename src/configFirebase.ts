4399b7bcf0fad1ca5478e58a13c2b282f4129101
Iqbal <iqbalmuthahhary@gmail.com>
2024-01-19 20:50:35 +0700

    add: env and change title web

--- src/configFirebase.ts
+++ src/configFirebase.ts
@@ -6,12 +6,12 @@ import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyAADvB-xIsopA3mfU1lHVYfW1V5UDSlWQk",
-  authDomain: "nowted-me.firebaseapp.com",
-  projectId: "nowted-me",
-  storageBucket: "nowted-me.appspot.com",
-  messagingSenderId: "1008780008918",
-  appId: "1:1008780008918:web:9ca6bbb9066eb03a8ed877",
+  apiKey: `${import.meta.env.VITE_APIKEY_FIREBASE}`,
+  authDomain: `${import.meta.env.VITE_AUTHDOMAIN_FIREBASE}`,
+  projectId: `${import.meta.env.VITE_PROJECTID_FIREBASE}`,
+  storageBucket: `${import.meta.env.VITE_STORAGEBUCKET_FIREBASE}`,
+  messagingSenderId: `${import.meta.env.VITE_MSG_FIREBASE}`,
+  appId: `${import.meta.env.VITE_APPID_FIREBASE}`
 };
 
 // Initialize Firebase
